7cc0936dd9d7beaca6cb5c873c599211fe8e8bd8
IT21810732 <it21810732@my.sliit.lk>
2025-03-21 16:58:05 +0530

    update 1.2 (add linked link, update about me )

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,31 @@
 import logo from "../assets/cplogo.png";
-import {FaLinkedin} from "react-icons/fa";
-import {FaGithub} from "react-icons/fa";
-import {FaSquareXTwitter} from "react-icons/fa6";
-import {FaInstagram} from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 
 const Navbar = () => {
     return (
         <nav className="mb-20 flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
-                <img className="w-25" src={logo} alt="logo"/>
+                <img className="w-25" src={logo} alt="logo" />
             </div>
 
             <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-                <FaLinkedin/>
-                <FaGithub/>
-                <FaSquareXTwitter/>
-                <FaInstagram/>
+                <a
+                    href="https://www.linkedin.com/in/chathuraprageeth/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:text-blue-700"
+                >
+                    <FaLinkedin />
+                </a>
+                <FaGithub />
+                <FaSquareXTwitter />
+                <FaInstagram />
             </div>
         </nav>
     );
-  };
-  
-  export default Navbar;
-  
+};
+
+export default Navbar;
